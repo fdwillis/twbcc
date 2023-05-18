0f0f6ddbc5ed41caa2de93c31b674c3fca6c45b0
Fred Willis <fdwillis7@icloud.com>
2023-05-18 08:05:43 -0500

    getting pricing tables ready

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,5 +1,5 @@
 require 'clockwork'
 include Clockwork
 
-every(2.days, 'delete.discounts', :at => '23:59') {'rake generate:deleteDiscounts'}
+every(2.days, 'delete.discounts', :at => '23:00') {'rake generate:deleteDiscounts'}
 every(2.days, 'generate.discounts', :at => '00:00') {'rake generate:discounts'}
