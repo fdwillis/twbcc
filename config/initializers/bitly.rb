a643b8bdb442cb4e67004d405e4b750fe2f65f20
Fred W <fwillis@ioufinancial.com>
2023-05-20 11:24:21 -0500

    bitly error too many requests

--- config/initializers/bitly.rb
+++ config/initializers/bitly.rb
@@ -1,6 +1,6 @@
-oauth = Bitly::OAuth.new(client_id: ENV['bitlyClient'], client_secret: ENV['bitlySecret'])
-@bitlyToken = oauth.access_token(username: 'team@oarlin.com', password: '9cC4WXuz.')
+# oauth = Bitly::OAuth.new(client_id: ENV['bitlyClient'], client_secret: ENV['bitlySecret'])
+# @bitlyToken = oauth.access_token(username: 'team@oarlin.com', password: '9cC4WXuz.')
 
-@bitlyClient = Bitly::API::Client.new(
-  token: ENV['bitlyToken']
-)
+# @bitlyClient = Bitly::API::Client.new(
+#   token: ENV['bitlyToken']
+# )
