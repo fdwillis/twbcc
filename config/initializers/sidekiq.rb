097b46aa65e9f809da044d681e1b4c6ea9c1af54
Fred W <fwillis@ioufinancial.com>
2023-06-10 04:20:32 -0500

    redis heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,13 @@
-
-Sidekiq.strict_args!(false)
-Sidekiq.configure_client do |config|
- config.redis = {db: 1}
-end
-
 Sidekiq.configure_server do |config|
- config.redis = {db: 1}
+  config.redis = {
+    url: ENV["REDIS_URL"],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
+
+Sidekiq.configure_client do |config|
+config.redis = {
+    url: ENV["REDIS_URL"],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+}
+
