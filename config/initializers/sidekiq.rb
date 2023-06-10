a7925004b559802d98c5f276332e5f9edccdf723
Fred W <fwillis@ioufinancial.com>
2023-06-10 04:24:11 -0500

    sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,8 +6,8 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-config.redis = {
-    url: ENV["REDIS_URL"],
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-}
-
+  config.redis = {
+      url: ENV["REDIS_URL"],
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
