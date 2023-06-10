79c79cfea5fdc2699e85fad4a57ad79de8e3363c
Fred W <fwillis@ioufinancial.com>
2023-06-10 15:25:42 -0500

    refactor soon

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :referredBy, :session,:ticker, :tickerType, :type, :direction, :timeframe, :currentPrice, :highPrice, :tradingview, :traderID, :lowPrice, :broker, :allowMarketOrder, :profitBy, :maxRisk, :perEntry, :entries => []]
+Rails.application.config.filter_parameters += [:uuid, :password, :referredBy, :session,:ticker, :tickerType, :type, :direction, :timeframe, :currentPrice, :highPrice, :tradingview, :traderID, :lowPrice, :broker, :allowMarketOrder, :profitBy, :maxRisk, :perEntry, :entries => []]
