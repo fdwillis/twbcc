f370e1b7a7f46716c87c3e65c0ae296bbe4fd811
Fred W <fwillis@ioufinancial.com>
2023-06-24 09:27:18 -0500

    all ready for other broker connections

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,41 +3,44 @@ class ApplicationRecord < ActiveRecord::Base
 
 	def self.trailStop(tvData, apiKey = nil, secretKey = nil)
 		puts "\n-- Current Price: #{tvData['currentPrice'].to_f.round(1)} --\n"
+
+		# pull bot trades
 		case true
 		when tvData['broker'] == 'KRAKEN'
 			openTrades = User.find_by(krakenLiveAPI: apiKey).trades.where(status: 'open', broker: tvData['broker'])
-			
 		end
+
+
+		# update trade status
 		if openTrades.present? && openTrades.size > 0 
 	  	openTrades.each do |trade|
-	  		
 	  		case true
 				when tvData['broker'] == 'KRAKEN'
 		  		requestK = Kraken.orderInfo(trade.uuid, apiKey, secretKey)
 		  		trade.update(status: requestK['status'])
-					
+		  		if trade.status == 'canceled'
+		  			trade.destroy
+			  	end
 				end
-	  		if trade.status == 'canceled'
-	  			trade.destroy
-		  	end
 	  	end
   	end
-  	
-  	# pull current pending orders for sync of database
+  	# pull closed/filled bot trades
   	case true
 		when tvData['broker'] == 'KRAKEN'
 	  	afterUpdates = User.find_by(krakenLiveAPI: apiKey).trades.where(status: 'closed', broker: tvData['broker'], finalTakeProfit: nil)
 		end
   	
+  	# protect closed/filled bot trades
   	if afterUpdates.present? && afterUpdates.size > 0	
 	  	afterUpdates.each do |tradeX|
 			  
 	    	case true
 				when tvData['broker'] == 'KRAKEN'	
 		  		requestOriginalE = Kraken.orderInfo(tradeX.uuid, apiKey, secretKey)
+		  		originalPrice = requestOriginalE['price'].to_f
+		  		originalVolume = requestOriginalE['vol'].to_f
 				end
 
-	  		originalPrice = requestOriginalE['price'].to_f
 	  		profitTrigger = originalPrice * (0.01 * tvData['profitTrigger'].to_f)
 	  		volumeTallyForTradex = 0
 	  		openProfitCount = 0
@@ -67,31 +70,35 @@ class ApplicationRecord < ActiveRecord::Base
 								when tvData['broker'] == 'KRAKEN'	
 						  		requestProfitTradex = Kraken.orderInfo(profitTrade.uuid, apiKey, secretKey)
 						  		profitTrade.update(status: requestProfitTradex['status'])
+						  		volumeForProfit = requestProfitTradex['vol'].to_f
+						  		priceToBeat = requestProfitTradex['descr']['price2'].to_f
 								end
 
-					  		if profitTrade.status == 'open'
-					  			volumeTallyForTradex += requestProfitTradex['vol'].to_f
+					  		if profitTrade.status == 'open' #or other status from oanda/alpaca
+					  			volumeTallyForTradex += volumeForProfit
 				  				openProfitCount += 1
 					  			
-						  		if (tvData['currentPrice'].to_f - (tvData['currentPrice'].to_f * (0.01 * tvData['trail'].to_f))).round(1) >  requestProfitTradex['descr']['price2'].to_f + ((0.01 * tvData['trail'].to_f) * (requestProfitTradex['descr']['price2'].to_f).round(1).to_f)
-						  			
-						  			orderParams = {
-									    "txid" 			=> profitTrade.uuid,
-									  }
-								  	routeToKraken = "/0/private/CancelOrder"
-								  	
-								  	cancel = Kraken.request(routeToKraken, orderParams, apiKey, secretKey)
-								  	profitTrade.destroy
-						  			puts "\n-- Old Take Profit Canceled --\n"
-						  			
-						  			@protectTrade = Kraken.newTrail(tvData,requestOriginalE, apiKey, secretKey, tradeX)
-						  			if @protectTrade.present? && @protectTrade['result']['txid'].present?
-						  				puts "\n-- Repainting Take Profit #{@protectTrade['result']['txid'][0]} --\n"
-						  			end
+						  		if (tvData['currentPrice'].to_f - (tvData['currentPrice'].to_f * (0.01 * tvData['trail'].to_f))).round(1) >  priceToBeat + ((0.01 * tvData['trail'].to_f) * (priceToBeat).round(1).to_f)
+						  			case true
+										when tvData['broker'] == 'KRAKEN'
+							  			krakenOrderParams = {
+										    "txid" 			=> profitTrade.uuid,
+										  }
+									  	routeToKraken = "/0/private/CancelOrder"
+									  	
+									  	cancel = Kraken.request(routeToKraken, krakenOrderParams, apiKey, secretKey)
+									  	profitTrade.destroy
+							  			puts "\n-- Old Take Profit Canceled --\n"
+							  			
+							  			@protectTrade = Kraken.newTrail(tvData,requestOriginalE, apiKey, secretKey, tradeX)
+							  			if @protectTrade.present? && @protectTrade['result']['txid'].present?
+							  				puts "\n-- Repainting Take Profit #{@protectTrade['result']['txid'][0]} --\n"
+							  			end
+										end
 						  		end
-					  		elsif profitTrade.status == 'closed'
-						  		volumeTallyForTradex += requestProfitTradex['vol'].to_f
-					  		elsif profitTrade.status == 'canceled'
+					  		elsif profitTrade.status == 'closed' #or other status from oanda/alpaca
+						  		volumeTallyForTradex += volumeForProfit
+					  		elsif profitTrade.status == 'canceled' #or other status from oanda/alpaca
 				  				puts "\n-- Removing Canceled Order #{profitTrade.uuid} --\n"
 					  			profitTrade.destroy
 					  			next
@@ -99,36 +106,38 @@ class ApplicationRecord < ActiveRecord::Base
 				  		end
 				  		
 			  			
-				  		if volumeTallyForTradex < requestOriginalE['vol'].to_f
+				  		if volumeTallyForTradex < originalVolume
 				  			if openProfitCount == 0
-					  			@protectTrade = Kraken.newTrail(tvData,requestOriginalE, apiKey, secretKey, tradeX)
-					  			if @protectTrade.present? && @protectTrade['result']['txid'].present?
-					  				puts "\n-- Additional Take Profit #{@protectTrade['result']['txid'][0]} --\n"
-					  			end
+				  				case true
+									when tvData['broker'] == 'KRAKEN'
+						  			@protectTrade = Kraken.newTrail(tvData,requestOriginalE, apiKey, secretKey, tradeX)
+						  			if @protectTrade.present? && @protectTrade['result']['txid'].present?
+						  				puts "\n-- Additional Take Profit #{@protectTrade['result']['txid'][0]} --\n"
+						  			end
+									end
 					  		else
 					  				puts "\n-- Waiting To Close Open Take Profit --\n"
 				  			end
 				  		else
-				  			checkFill = Kraken.orderInfo(tradeX.take_profits.last.uuid, apiKey, secretKey)
-					  		tradeX.take_profits.last.update(status: checkFill['status'])
-
-					  		if checkFill['status'] == 'closed'
-						  		tradeX.update(finalTakeProfit: tradeX.take_profits.last.uuid)
-						  		puts "\n-- Position Closed #{tradeX.uuid} --\n"
-				  				puts "\n-- Last Profit Taken #{tradeX.take_profits.last.uuid} --\n"
-					  		elsif checkFill['status'] == 'open'
-					  			tradeX.update(finalTakeProfit:nil)
-				  				puts "\n-- Waiting To Close Last Position --\n"
-					  		end
+				  			case true
+								when tvData['broker'] == 'KRAKEN'
+					  			checkFill = Kraken.orderInfo(tradeX.take_profits.last.uuid, apiKey, secretKey)
+						  		tradeX.take_profits.last.update(status: checkFill['status'])
+						  		if checkFill['status'] == 'closed'
+							  		tradeX.update(finalTakeProfit: tradeX.take_profits.last.uuid)
+							  		puts "\n-- Position Closed #{tradeX.uuid} --\n"
+					  				puts "\n-- Last Profit Taken #{tradeX.take_profits.last.uuid} --\n"
+						  		elsif checkFill['status'] == 'open'
+						  			tradeX.update(finalTakeProfit:nil)
+					  				puts "\n-- Waiting To Close Last Position --\n"
+						  		end
+								end
 				  		end
 
 				  	end
 			  	end
 				when tvData['direction'] == 'buy'
-					profitTriggerPassed = (originalPrice - profitTrigger).round(1).to_f
-					if tvData['currentPrice'].to_f < profitTriggerPassed - ((0.01 * tvData['trail'].to_f) * profitTriggerPassed)
-						
-					end
+					
 				end
 	  	end
   	end
@@ -168,51 +177,51 @@ class ApplicationRecord < ActiveRecord::Base
 
 	  			priceToSet = (tvData['direction'] == 'sell' ? tvData['highPrice'].to_f + (tvData['highPrice'].to_f * (0.01 * entryPercentage.to_f)) : tvData['lowPrice'].to_f - (tvData['lowPrice'].to_f * (0.01 * entryPercentage.to_f))).round(1)
 
-				  case true
-				  when tvData['broker'] == 'KRAKEN'
-				    case true
-				    when tvData['ticker'] == 'BTCUSD'
-				    	@unitsFiltered = (@unitsToTrade > 0.0001 ? @unitsToTrade : 0.0001)
-				    end
+			    case true
+			    when tvData['ticker'] == 'BTCUSD'
+			    	@unitsFiltered = (@unitsToTrade > 0.0001 ? @unitsToTrade : 0.0001)
+			    end
 
-		  			@orderParams = {
+			    case true
+		  		when tvData['broker'] == 'KRAKEN'
+		  			krakenParams0 = {
 					    "pair" 			=> tvData['ticker'],
 					    "type" 			=> tvData['direction'],
 					    "ordertype" => "limit",
 					    "price" 		=> priceToSet,
 					    "volume" 		=> "#{@unitsFiltered}",
 					  }
+					end
 
-				  	if tvData['direction'] == 'buy' 
-						  case true
-						  when tvData['broker'] == 'KRAKEN'
-							  @requestK = Kraken.request('/0/private/AddOrder', @orderParams, apiKey, secretKey)
-						  	
-						  end
-				  	end
-				  	
-					  if tvData['direction'] == 'sell'
-						  case true
-						  when tvData['broker'] == 'KRAKEN'
-							  @requestK = Kraken.request('/0/private/AddOrder', @orderParams, apiKey, secretKey)
-						  	
-						  end
-					  end
-
+			  	if tvData['direction'] == 'buy' 
 					  case true
 					  when tvData['broker'] == 'KRAKEN'
-						  if @requestK.present? && @requestK['result'].present?
-							  if @requestK['result']['txid'].present?
-							  	User.find_by(krakenLiveAPI: apiKey).trades.create(uuid:  @requestK['result']['txid'][0], broker: tvData['broker'], direction: tvData['direction'], status: 'open')
-							  	puts "\n-- Kraken Entry Submitted --\n"
-							  end
-						  else
-							  if @requestK['error'][0].present? && @requestK['error'][0].include?("Insufficient")
-							  	puts "\n-- MORE CASH FOR ENTRIES --\n"
-								else
-							  	puts "\n-- Waiting For Better Entry --\n"
-								end
+						  @requestK = Kraken.request('/0/private/AddOrder', krakenParams0, apiKey, secretKey)
+					  	
+					  end
+			  	end
+			  	
+				  if tvData['direction'] == 'sell'
+					  case true
+					  when tvData['broker'] == 'KRAKEN'
+						  @requestK = Kraken.request('/0/private/AddOrder', krakenParams0, apiKey, secretKey)
+					  	
+					  end
+				  end
+
+				  case true
+				  when tvData['broker'] == 'KRAKEN'
+					  if @requestK.present? && @requestK['result'].present?
+						  if @requestK['result']['txid'].present?
+						  	User.find_by(krakenLiveAPI: apiKey).trades.create(uuid:  @requestK['result']['txid'][0], broker: tvData['broker'], direction: tvData['direction'], status: 'open')
+						  	puts "\n-- Kraken Entry Submitted --\n"
 						  end
+					  else
+						  if @requestK['error'][0].present? && @requestK['error'][0].include?("Insufficient")
+						  	puts "\n-- MORE CASH FOR ENTRIES --\n"
+							else
+						  	puts "\n-- Waiting For Better Entry --\n"
+							end
 					  end
 				  end
 
