9563c53a0bc90d8cfb37a071ef5c6b13c65917b2
Fred W <fwillis@ioufinancial.com>
2023-06-25 00:28:05 -0500

    filter keys

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:uuid, :password, :referredBy, :session, :tradingview, :traderID]
+Rails.application.config.filter_parameters += [:krakenLiveAPI, :krakenLiveSecret, :uuid, :password, :referredBy, :session, :tradingview, :traderID]
