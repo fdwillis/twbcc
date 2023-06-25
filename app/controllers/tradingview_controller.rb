f409ef5863e698549e411b76e7d4c12b61aaa083
Fred W <fwillis@ioufinancial.com>
2023-06-25 08:38:29 -0500

    moved deleting canceled trades to the background

--- app/controllers/tradingview_controller.rb
+++ app/controllers/tradingview_controller.rb
@@ -96,8 +96,6 @@ class TradingviewController < ApplicationController
 					end
 				end
 
-				Trade.where(status: 'canceled').destroy_all
-
 				render json: {success: true}
 			else
 				puts "\n-- No Trader Found --\n"
