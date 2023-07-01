0f3eaa6aeb99c285bcb4a69c87439720ea007f78
Fred W <fwillis@ioufinancial.com>
2023-06-30 20:45:45 -0500

    index

--- db/migrate/20230701012106_add_uniqness_to_things.rb
+++ db/migrate/20230701012106_add_uniqness_to_things.rb
@@ -1,14 +1,27 @@
 class AddUniqnessToThings < ActiveRecord::Migration[6.0]
-  def change
-    add_index :users, [:krakenLiveAPI], unique: true
+  def up
+    change_column :users, [:krakenLiveAPI], unique: true
 
-    add_index :users,[:krakenLiveSecret], unique: true
-    add_index :users, [:krakenTestAPI], unique: true
-    add_index :users, [:krakenTestSecret], unique: true
-    add_index :users, [:alpacaKey], unique: true
-    add_index :users, [:alpacaSecret], unique: true
-    add_index :users,  [:alpacaTestKey], unique: true
-    add_index :users, [:alpacaTestSecret], unique: true
-    add_index :users, [:oandaToken], unique: true
+    change_column :users,[:krakenLiveSecret], unique: true
+    change_column :users, [:krakenTestAPI], unique: true
+    change_column :users, [:krakenTestSecret], unique: true
+    change_column :users, [:alpacaKey], unique: true
+    change_column :users, [:alpacaSecret], unique: true
+    change_column :users,  [:alpacaTestKey], unique: true
+    change_column :users, [:alpacaTestSecret], unique: true
+    change_column :users, [:oandaToken], unique: true
+  end
+
+  def down
+    remove_index :users, [:krakenLiveAPI], unique: true
+
+    remove_index :users,[:krakenLiveSecret], unique: true
+    remove_index :users, [:krakenTestAPI], unique: true
+    remove_index :users, [:krakenTestSecret], unique: true
+    remove_index :users, [:alpacaKey], unique: true
+    remove_index :users, [:alpacaSecret], unique: true
+    remove_index :users,  [:alpacaTestKey], unique: true
+    remove_index :users, [:alpacaTestSecret], unique: true
+    remove_index :users, [:oandaToken], unique: true
   end
 end
