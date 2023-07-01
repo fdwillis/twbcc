66c111d5d4021fd6529d2019e1b73b58ade54c00
Fred W <fwillis@ioufinancial.com>
2023-07-01 18:56:56 -0500

    memberhip

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,8 +1,8 @@
 require 'clockwork'
 include Clockwork
 
-every(2.days, 'delete.discounts', :at => '23:00') {'rake generate:deleteDiscounts'}
-every(2.days, 'generate.discounts', :at => '00:00') {'rake generate:discounts'}
+# every(2.days, 'delete.discounts', :at => '23:00') {'rake generate:deleteDiscounts'}
+# every(2.days, 'generate.discounts', :at => '00:00') {'rake generate:discounts'}
 
 # every(1.day, 'trading.referrals', :at => '00:00') {'rake generate:tradingReferrals'}
 # every(1.day, 'trading.referrals', :at => '06:00') {'rake generate:tradingReferrals'}
