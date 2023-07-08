f480e06306d43b7fcdd7cf620f02066f671ca512
Fred W <fwillis@ioufinancial.com>
2023-07-08 10:59:14 -0500

    calling tradier for users via tradierToken

--- app/models/tradiers.rb
+++ app/models/tradiers.rb
@@ -1,7 +1,11 @@
 class Tradiers < ApplicationRecord
 
   def self.tradierRequest(token)
-		@tradier = Tradier::Client.new(access_token: ENV['tradierToken'])
+		@tradier = Tradier::Client.new(access_token: token)
+  end
+
+  def self.tradierStock(symbol, token)
+		 tradierRequest(token).quote(symbol.upcase)
   end
 
 	def self.optionsThisFriday(symbol,token)
@@ -9,6 +13,7 @@ class Tradiers < ApplicationRecord
 	end
 
 	def self.upcomingFriday
+		# allow to pull from multiple weeks out
 		Date.today.next_occurring(:friday).strftime("%Y-%m-%d")
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_07_174157) do
+ActiveRecord::Schema.define(version: 2023_07_08_152500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -199,6 +199,7 @@ ActiveRecord::Schema.define(version: 2023_07_07_174157) do
     t.float "profitTrigger"
     t.float "maxRisk"
     t.string "allowMarketOrder"
+    t.string "tradierToken"
     t.index ["alpacaKey"], name: "index_users_on_alpacaKey", unique: true
     t.index ["alpacaSecret"], name: "index_users_on_alpacaSecret", unique: true
     t.index ["alpacaTestKey"], name: "index_users_on_alpacaTestKey", unique: true
