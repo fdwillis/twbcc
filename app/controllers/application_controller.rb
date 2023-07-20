a7763fb1a3e6f79ca35692651445ebe77af9fdf2
Fred W <fwillis@ioufinancial.com>
2023-07-20 08:36:09 -0500

    welcome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -648,7 +648,7 @@ class ApplicationController < ActionController::Base
       {prob:'Too Busy Working?', solu: "Oarlin stays active 24/7 to reach your financial goals while you are busy"},
       {prob:'Too Busy With Home?', solu: "Oarlin stays active 24/7 to reach your financial goals while you are busy"},
       {prob:'Too Busy For Investing?', solu: 'Oarlin secures your investment goals so that you can secure your life goals'},
-      {prob:'Hesitating To Execute Trades?', solu: 'Oarlin will only trade when the time is right'},
+      {prob:'Volitility Making You Hesitate?', solu: 'Oarlin will only trade when the time is right'},
       {prob:"Still Learning About Investing?", solu: 'Oarlin will make you money while you learn to invest'},
       {prob:"The Only Social Platform For Trading", solu: 'Oarlin is the home all traders have been waiting for'},
     ] 
